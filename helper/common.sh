492f9c4d6a586eadb5b1eb584268bdb858e56e67
jiangyong <jiangyong17@huawei.com>
2016-12-24 10:20:00 +0800

    add common.sh function

--- helper/common.sh
+++ helper/common.sh
@@ -4,22 +4,10 @@
 PID=$$
 SCRIPT_NAME=`readlink /proc/${PID}/fd/255 | awk -F \/ {'print $NF'}`
 SCRIPT_PATH=`readlink /proc/${PID}/fd/255 | awk -F ${SCRIPT_NAME} {'print $1'}`
-SCRIPT_IP=`netstat -tln|grep "36000"|awk '{print $4}'|awk -F":" '{print $1}'`
 
-#打印日志
-function INFO()
-{
-    now=`date "+[%Y-%m-%d %H:%M:%S]"`
-    aashell_file_name=${0##*/}
-    echo -e "$now[INFO][${aashell_file_name}:$BASH_LINENO] $1"
-}
-
-function ERROR()
-{
-    now=`date "+[%Y-%m-%d %H:%M:%S]"`
-    echo -e "$now[ERROR][$0:$BASH_LINENO] $1"
-}
-
+#******************************************************************#
+# Description: set value through keyname to ini config file
+#*****************************************************
 function SETV
 {
     filename=$1
@@ -34,17 +22,68 @@ function SETV
     fi
 }
 
-function SENDRTX()
-{
-    MSG_RECVER="$1"
-    MSG_SUBJECT="$2"
-    MSG_BODY="$3"
-    now=`date "+%Y-%m-%d %H:%M:%S"`
-    SENDRTX_EXE="/usr/local/bin/sendrtxproxy"
-    if [ ! -f ${SENDRTX_EXE} ] ;then
-        ERROR "${SENDRTX_EXE} is not exist!"
-        return
-    fi
-    ${SENDRTX_EXE} "jasonyjiang" "${MSG_RECVER}" "${MSG_SUBJECT}" "${MSG_BODY}===[${now}][$SCRIPT_IP]" 0 >/dev/null
-    ${SENDRTX_EXE} "jasonyjiang" "${MSG_RECVER}" "${MSG_SUBJECT}" "${MSG_BODY}===[${now}][$SCRIPT_IP]" 1 >/dev/null
+#******************************************************************#
+# Description: print infomatioin message by green to terminal
+#*******************************************************
+function TERM_INFO()
+{  
+    local now=`date +"%Y-%m-%d %H:%M:%S"`
+    local file=${0##*/}
+    echo -en "\e[0;32;1m"
+    echo -e "[$now][INFO][${file}:$BASH_LINENO] $1"
+    echo -en "\e[0m"
+}
+
+#******************************************************************#
+# Description: print error message by red to terminal
+#*******************************************************
+function TERM_ERROR()
+{  
+    local now=`date +"%Y-%m-%d %H:%M:%S"`
+    local file=${0##*/}
+    echo -en "\e[0;31;1m"
+    echo -e "[$now][INFO][${file}:$BASH_LINENO] $1"
+    echo -en "\e[0m"
+}
+
+#******************************************************************#
+# Description: print warnning message by yellow to terminal
+#*******************************************************
+function TERM_WARN()
+{  
+    local now=`date +"%Y-%m-%d %H:%M:%S"`
+    local file=${0##*/}
+    echo -en "\e[0;33;1m"
+    echo -e "[$now][INFO][${file}:$BASH_LINENO] $1"
+    echo -en "\e[0m"
+}
+
+#******************************************************************#
+# Description: print infomatioin message 
+#*******************************************************
+function LOG_INFO()
+{  
+    local now=`date +"%Y-%m-%d %H:%M:%S"`
+    local file=${0##*/}
+    echo -e "[$now][INFO][${file}:$BASH_LINENO] $1"
+}
+
+#******************************************************************#
+# Description: print error message 
+#*******************************************************
+function LOG_ERROR()
+{  
+    local now=`date +"%Y-%m-%d %H:%M:%S"`
+    local file=${0##*/}
+    echo -e "[$now][ERROR][${file}:$BASH_LINENO] $1"
+}
+
+#******************************************************************#
+# Description: print warnning message 
+#*******************************************************
+function LOG_WARN()
+{  
+    local now=`date +"%Y-%m-%d %H:%M:%S"`
+    local file=${0##*/}
+    echo -e "[$now][WARN][${file}:$BASH_LINENO] $1"
 }
